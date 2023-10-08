c60837f8bd11bf1f198cb6ec3f327f552bcf7223
M-ilyas1 <mohammadilyas6800@gmail.com>
2023-10-08 17:47:50 +0500

    create a tabel, fetch data from DataBase, also add search filter

--- Team/Ilyas_code/connnection.php
+++ Team/Ilyas_code/connnection.php
@@ -1,11 +1,9 @@
 <?php
-// Database credentials
 $servername = "localhost";
 $username = "root";
 $password = "";
 $database = "student_data";
 
-// Create a connection to the database
 $conn = new mysqli($servername, $username, $password, $database);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -16,20 +14,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $marks = $_POST["marks"];
     $city = $_POST["city"];
 
-    // Create a prepared statement
     $stmt = $conn->prepare("INSERT INTO student (name, class, rollNo, age, marks, city) VALUES (?, ?, ?, ?, ?, ?)");
     
-    // Bind parameters
     $stmt->bind_param("ssssss", $name, $class, $rollNo, $age, $marks, $city);
 
-    // Execute the statement
     if ($stmt->execute()) {
-        echo "Record added successfully.";
+        $insert = false;
     } else {
         echo "Error: " . $stmt->error;
     }
-
-    // Close the statement
+    
     $stmt->close();
+}
+?>
+
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "student_data";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
